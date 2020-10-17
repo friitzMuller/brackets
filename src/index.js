f1e533c516a7a24c969c926ebdba5cb53c2b6528
Андрей <dronxxx1993@yandex.ru>
2020-10-18 02:08:47 +0400

    feat :resolve task breckets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(arr, bracketsConfig) {
+  let strArr  = bracketsConfig;
+  let open = [];
+  let close = [];
+  let stack = [];
+
+   for (var i = 0;i<strArr.length;i++){
+        open.push(strArr[i][0])
+        close.push(strArr[i][1])
+   }
+
+     for (let j = 0; j<arr.length; j++){
+        if(open.includes(arr[j])){
+            if(close.includes(arr[j]) && stack[stack.length-1] == arr[j]){
+                stack.pop()
+            }else{
+                stack.push(arr[j])
+            }
+        }else{
+            if(close.indexOf(arr[j]) === open.indexOf(stack[stack.length - 1])){
+                    stack.pop()
+            }else{
+                return false;
+            }
+        }
+    }
+
+    return stack.length == 0
+
+ }
+
